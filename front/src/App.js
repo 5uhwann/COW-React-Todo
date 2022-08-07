c83fd0ef8a5f9544e16890a430b6ceccda6b6834
5uhwann <5uhwann27@gmail.com>
2022-08-07 21:46:37 +0900

    fix: TodoListItem map렌더링 오류 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,5 @@
 import './App.css';
-import AddButton from './component/ui/AddButton';
-import TodoListItem from './component/todoList/TodoListItem';
-import TodoList from './component/todoList/TodoList';
-import Menu from './component/menu/Menu';
 import MainPage from './component/pages/MainPage';
-
-const data = [
-  {
-    "id": 1,
-    "content": "test1",
-    "isCompleted": false,
-    "createdAt": "2022-07-26T00:14:52.449Z",
-    "updatedAt": "2022-07-26T00:15:20.000Z"
-  },
-  {
-    "id": 2,
-    "content": "test2",
-    "isCompleted": true,
-    "createdAt": "2022-07-26T00:14:52.449Z",
-    "updatedAt": "2022-07-26T00:15:20.000Z"
-  },
-  {
-    "id": 3,
-    "content": "test3",
-    "isCompleted": false,
-    "createdAt": "2022-07-26T00:14:52.449Z",
-    "updatedAt": "2022-07-26T00:15:20.000Z"
-  }
-]
 
 function App() {
   return (
